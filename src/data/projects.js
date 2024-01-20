d8dbba7c5f4c3abdad14aa29d0e7bc360e37c01a
Fontas <xenmoraitis@gmail.com>
2024-01-20 14:30:08 +0100

    Add cupculator link to github

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,6 +2,7 @@ export default [
   {
     name: 'cupculator',
     description: `A Progressive web app that helps you calculate the quantity of materials used in cooking recipes. Installable PWA with REACT | Vercel | Redis KV`,
+    github: 'https://github.com/fontas-moraitis/cupculator',
     live: '//www.cupculator.com',
     logo: '01'
   },
