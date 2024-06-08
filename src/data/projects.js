7787caf3b198bdd10c1be2110abc592678f28cdc
Fontas <xenmoraitis@gmail.com>
2024-06-08 09:59:18 +0200

    feat: added mnimia to projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,28 +1,34 @@
 export default [
   {
+    name: 'mnimia',
+    description: 'An iOS application for excersing your memory. Build with React-native.',
+    live: 'https://apps.apple.com/nl/app/mnimia/id6502452878?l=en-GB',
+    logo: '01'
+  },
+  {
     name: 'feedbackjar',
     description: `An AI assisted way of gathering and analyzing feedback`,
     live: '//www.feedbackjar.com',
-    logo: '01'
+    logo: '02'
   },
   {
     name: 'cupculator',
     description: `A Progressive web app that helps you calculate the quantity of materials used in cooking recipes. Installable PWA with REACT | Vercel | Redis KV`,
     github: 'https://github.com/fontas-moraitis/cupculator',
     live: '//www.cupculator.com',
-    logo: '02'
+    logo: '03'
   },
   {
     name: 'stavrosperakis.com',
     description: `A gallery web site for a sculptor and artist in Athens, content is handled with Storyblok. React | NextJS.`,
     github: 'https://github.com/fontas-moraitis/Gallery-eshop',
     live: '//www.stavrosperakis.com',
-    logo: '03'
-  }, 
+    logo: '04'
+  },
   {
     name: 'fontas.me',
     description: `Link to the github repository of the current page. Vue 3 | Vite | Gsap`,
     github: 'https://fontas-me.vercel.app',
-    logo: '04'
+    logo: '05'
   }
 ]
